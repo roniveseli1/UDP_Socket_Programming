08f51a840e9e5e8f0979930b9fbf7723ac7aecd1
Shendrit Hyseni <138986507+Shendrit-Hy@users.noreply.github.com>
2024-11-10 14:02:59 +0100

    Initial SERVER.cpp and CLIENT.cpp

---
+++ UDPServer.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <fstream>
+#include <cstdlib>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <set>
+#include <unordered_map>
+#include <ctime>
+#include <thread>
+#include <dirent.h>
+
+using namespace std;
+
+int main(){
+    return 0;
+}
