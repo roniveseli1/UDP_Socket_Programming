b7462b79fe98f5fa10716cd992d25d62fb67d835
Shendrit Hyseni <138986507+Shendrit-Hy@users.noreply.github.com>
2024-11-10 17:01:49 +0100

    Update UDPClient.cpp

--- UDPClient.cpp
+++ UDPClient.cpp
@@ -9,5 +9,25 @@
 using namespace std;
 
 int main() {
-    
+    const char* serverIp = "127.0.0.1";
+    const int serverPort = 1200;
+    const int bufferSize = 2048;
+
+    int clientSocket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (clientSocket < 0) {
+        cerr << "Failed to create socket." << endl;
+        return 1;
+    }
+
+    sockaddr_in serverAddr = {};
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(serverPort);
+    inet_pton(AF_INET, serverIp, &serverAddr.sin_addr);
+
+    while (true) {
+        
+    }
+
+    close(clientSocket);
+    return 0;
 }
