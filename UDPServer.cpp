2bcc65e0a0db7b6844c6c9c6ac6b457c1f2185e3
roniveseli1 <roni.veseli@student.uni-pr.edu>
2024-11-10 16:44:00 +0100

    Added handleRead function

--- UDPServer.cpp
+++ UDPServer.cpp
@@ -15,6 +15,29 @@
 
 using namespace std;
 
+void handleRead(int serverSocket, const string &filename, sockaddr_in &clientAddr) {
+    ifstream inFile(filename);
+    if (!inFile) {
+        sendMessageToClient(serverSocket, "Error: Could not open the file: " + filename, clientAddr);
+        return;
+    }
+
+    string line, fileContent;
+    while (getline(inFile, line)) {
+        fileContent += line + "\n";
+    }
+    inFile.close();
+
+    if (!fileContent.empty()) {
+        sendMessageToClient(serverSocket, fileContent, clientAddr);
+    } else {
+        sendMessageToClient(serverSocket, "The file is empty.", clientAddr);
+    }
+}
+
+
 int main(){
+    
+
     return 0;
 }
