4591bd423ccb169a2e0db2532cd587f660fff125
Shendrit Hyseni <138986507+Shendrit-Hy@users.noreply.github.com>
2024-11-10 17:16:29 +0100

    Update UDPClient.cpp

--- UDPClient.cpp
+++ UDPClient.cpp
@@ -25,7 +25,30 @@ int main() {
     inet_pton(AF_INET, serverIp, &serverAddr.sin_addr);
 
     while (true) {
-        
+        string userInput;
+        cout << "Enter a request or type 'exit' to close the connection: ";
+        getline(cin, userInput);
+
+        sendto(clientSocket, userInput.c_str(), userInput.size(), 0,
+               (sockaddr*)&serverAddr, sizeof(serverAddr));
+
+        if (userInput == "exit") {
+            break;
+        }
+
+        char buffer[bufferSize];
+        sockaddr_in fromAddr = {};
+        socklen_t fromAddrLen = sizeof(fromAddr);
+
+        int receivedBytes = recvfrom(clientSocket, buffer, bufferSize - 1, 0,
+                                     (sockaddr*)&fromAddr, &fromAddrLen);
+        if (receivedBytes < 0) {
+            cerr << "Failed to receive data." << endl;
+            continue;
+        }
+
+        buffer[receivedBytes] = '\0';
+        cout << "Data received from server: " << buffer << endl;
     }
 
     close(clientSocket);
